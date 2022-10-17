bff2d2ae8aada1e526551d28fdcc17ea44f0ce6c
Desh Deepak Dhruv <97728256+deshdeepak2019@users.noreply.github.com>
2022-10-18 00:01:07 +0530

    Time: 8 ms (28.88%), Space: 7.3 MB (6.60%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     bool checkIfPangram(string str) {
-        unordered_map<char,int>mp;
-        for(int i=0;i<str.size();i++){
-            mp[str[i]]++;
-        }
-        for(int i=0;i<26;i++){
-            char a='a'+i;
-            if(mp[a]==0){
-                return 0;
-            }
-        }
+//         unordered_map<char,int>mp;
+//         for(int i=0;i<str.size();i++){
+//             mp[str[i]]++;
+//         }
+//         for(int i=0;i<26;i++){
+//             char a='a'+i;
+//             if(mp[a]==0){
+//                 return 0;
+//             }
+//         }
         
-        return 1;
+        unordered_set<char>st(str.begin(),str.end());
+        if(st.size()==26){
+            return 1;
+        }
+        return 0;
     }
 };
