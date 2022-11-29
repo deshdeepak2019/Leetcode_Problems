5b674c47bf5b228002c128b58850009e90b75327
Desh Deepak Dhruv <97728256+deshdeepak2019@users.noreply.github.com>
2022-11-29 16:54:03 +0530

    Added solution - LeetHub

---
+++ Flood fill Algorithm - GFG/flood-fill-algorithm.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+void dfs(int row,int col,vector<vector<int>>&ans,vector<vector<int>>&image,
+           int newColor,int delRow[],int delCol[],int iniColor){
+               int n=image.size();
+               int m=image[0].size();
+               ans[row][col]=newColor;
+               for(int i=0;i<4;i++){
+                   int nrow=row+delRow[i];
+                    int ncol=col+delCol[i];
+         if(nrow>=0 and nrow<n and ncol>=0 and ncol<m and image[nrow][ncol]==iniColor 
+                   and ans[nrow][ncol]!=newColor){
+                       dfs(nrow,ncol,ans,image,newColor,delRow,delCol,iniColor);
+                   }
+               }
+           }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+           int iniColor=image[sr][sc];
+           int delRow[]={-1,0,+1,0};
+           int delCol[]={0,+1,0,-1};
+           vector<vector<int>>ans=image;
+           dfs(sr,sc,ans,image,newColor,delRow,delCol,iniColor);
+           return ans;
+    }
+};
+ 
+ 
+
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>image(n, vector<int>(m,0));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++)
+				cin >> image[i][j];
+		}
+		int sr, sc, newColor;
+		cin >> sr >> sc >> newColor;
+		Solution obj;
+		vector<vector<int>> ans = obj.floodFill(image, sr, sc, newColor);
+		for(auto i: ans){
+			for(auto j: i)
+				cout << j << " ";
+			cout << "\n";
+		}
+	}
+	return 0;
+}
+// } Driver Code Ends
