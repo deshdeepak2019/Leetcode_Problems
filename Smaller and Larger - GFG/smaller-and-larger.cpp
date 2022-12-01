c386790f88860a175f344a95546341413efb016a
Desh Deepak Dhruv <97728256+deshdeepak2019@users.noreply.github.com>
2022-12-01 16:51:43 +0530

    Added solution - LeetHub

---
+++ Smaller and Larger - GFG/smaller-and-larger.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	vector<int> getMoreAndLess(int arr[], int n, int x) {
+	    int c1=0,c2=0;
+	    for(int i=0;i<n;i++){
+	        if(arr[i]>=x){
+	            c1++;
+	        }
+	        if(arr[i]<=x){
+	            c2++;
+	        }
+	    }
+	    return {c2,c1};
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getMoreAndLess(arr, n, x);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
