b08fa4d768d4633f537f5144249ce684d113564c
Desh Deepak Dhruv <97728256+deshdeepak2019@users.noreply.github.com>
2023-02-06 23:23:31 +0530

    Time: 7 ms (62.32%), Space: 9.7 MB (61.93%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& dd, int n) {
-        int p=0,q=0;
-        // vector<int>v(dd.begin(),dd.begin()+n);
-        // vector<int>v1(dd.begin()+n,dd.end());
-        // vector<int>v2;
-        // for(int i=0;i<2*n;i++){
-        //     if(i%2==0){
-        //         v2.push_back(v[p]);
-        //             p++;
-        //     }else{
-        //         v2.push_back(v1[q]);
-        //         q++;
-        //     }
-        // }
-        // return v2;
         vector<int>v;
         for(int i=0;i<n;i++){
             v.push_back(dd[i]);
